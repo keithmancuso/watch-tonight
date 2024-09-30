6b4a7047fcd8aef4ba270ebc5c36117d0cb49381
Keith Mancuso <keithmancuso@gmail.com>
2024-09-30 14:07:57 -0700

    added a database config for storing users

--- auth.ts
+++ auth.ts
@@ -1,6 +1,13 @@
 import NextAuth from "next-auth"
 import Google from "next-auth/providers/google"
+import PostgresAdapter from "@auth/pg-adapter"
+import { Pool } from "pg"
+
+const pool = new Pool({
+  connectionString: process.env.POSTGRES_URL,
+})
  
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: PostgresAdapter(pool),
   providers: [Google],
 })
